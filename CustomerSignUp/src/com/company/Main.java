87c8ab33197d23acb1bbcb00d1444d1047ecb3c2
Sijjad Khalid <57273003+SijjadhKhalid@users.noreply.github.com>
2019-11-07 15:30:43 -0500

    Update Main.java

--- CustomerSignUp/src/com/company/Main.java
+++ CustomerSignUp/src/com/company/Main.java
@@ -1,5 +1,9 @@
 package com.company;
 /*
+Sijjad Khalid
+CustomerSignUp
+*/
+/*
 Purpose: Get the customers information and ask user whether or not they want to be on the mailing list
 Input: Enter name, address, number. Then input y/n to be on the mailing list
 Processing: information will be sent to another class with a constructor
